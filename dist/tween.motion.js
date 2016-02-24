8de09d40aea22fd97a1823860793b727ac17b3a4
Manuel <hola@manuelro.co>
2016-02-24 16:39:33 -0600

    Fixes function type checking
    Checks out the property type, if  'function' calls it with the defined
    value.

--- dist/tween.motion.js
+++ dist/tween.motion.js
@@ -9,21 +9,6 @@
 		if(!target){ target = {}; }
 		if(!time){ time = 0; }
 		if(!options){ options = {}; }
-		var propRec = function(context, route, value){
-			var paths = route.split('.');
-			var rec = function(context, i, isLast, value){
-				var path = paths[i];
-				if(paths.indexOf(path) === paths.length - 1){ isLast = true; }
-				if(isLast){
-					context[path] = value;
-				} else {
-					i++;
-					context = context[path];
-					rec(context, i, isLast, value);
-				}
-			};
-			rec(context, 0, false, value);
-		};
 		var onUpdate = function(){
 			for(var key in target) propRec(elem, key, start[key]);
 		};
@@ -32,11 +17,35 @@
 		options.callbacks.onUpdate = options.callbacks.onUpdate || onUpdate;
 		options.callbacks.onComplete = options.callbacks.onComplete || onComplete;
 		var tween = new TWEEN.Tween(start).to(target, time);
+		
 		for(var key in options){
-			if(key !== 'callbacks'){ tween[key] = options[key]; }
+			if(key !== 'callbacks'){ 
+				if(typeof tween[key] === 'function') {
+					tween[key](options[key]);
+				} else {
+					tween[key] = options[key]; 
+				}
+			}
 		}
+
 		tween.start();
 		tween.onUpdate(options.callbacks.onUpdate);
 		tween.onComplete(options.callbacks.onComplete);
 	}
+
+	var propRec = function(context, route, value){
+		var paths = route.split('.');
+		var rec = function(context, i, isLast, value){
+			var path = paths[i];
+			if(paths.indexOf(path) === paths.length - 1){ isLast = true; }
+			if(isLast){
+				context[path] = value;
+			} else {
+				i++;
+				context = context[path];
+				rec(context, i, isLast, value);
+			}
+		};
+		rec(context, 0, false, value);
+	};
 })();
