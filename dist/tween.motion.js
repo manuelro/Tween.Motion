3d6f14bdb7b572d7b340880cc9246cd21f788255
Manuel <hola@manuelro.co>
2016-03-07 13:23:52 -0600

    Adding Stagger and Batch functionality

--- dist/tween.motion.js
+++ dist/tween.motion.js
@@ -3,41 +3,43 @@
 	if(window.TWEEN){
 		window.TWEEN.Motion = Motion;
 		window.TWEEN.Stagger = Stagger;
+		window.TWEEN.Batch = Batch;
 	}
 
-	console.log(window.TWEEN);
+	function Motion(config){
+		config = config || {};
+		config.element = config.element || {};
+		config.from = config.from || {};
+		config.to = config.to || {};
+		config.duration = config.duration || 0;
+		config.autostart = typeof config.autostart === 'undefined' ? true : false;
 
-	function Motion(elem, start, target, time, options, autostart){
-		if(!elem){ elem = {}; }
-		if(!start){ start = {}; }
-		if(!target){ target = {}; }
-		if(!time){ time = 0; }
-		if(!options){ options = {}; }
-		if(typeof autostart === 'undefined' || typeof autostart === 'null'){ autostart = true; }
-
-		var onComplete = function(){};
-		options.callbacks = options.callbacks || {};
-		options.callbacks.onUpdate = options.callbacks.onUpdate || onUpdate;
-		options.callbacks.onComplete = options.callbacks.onComplete || onComplete;
-		var tween = new TWEEN.Tween(start).to(target, time);
+		config.options = config.options || {};
+		config.options.callbacks = config.options.callbacks || {};
+		config.options.callbacks.onUpdate = config.options.callbacks.onUpdate || onUpdate;
+		config.options.callbacks.onComplete = config.options.callbacks.onComplete || onComplete;
 		
-		for(var key in options){
+		var tween = new TWEEN.Tween(config.from).to(config.to, config.duration);
+		
+		for(var key in config.options){
 			if(key !== 'callbacks'){ 
 				if(typeof tween[key] === 'function') {
-					tween[key](options[key]);
+					tween[key](config.options[key]);
 				} else {
-					tween[key] = options[key]; 
+					tween[key] = config.options[key]; 
 				}
 			}
 		}
 
-		tween.onUpdate(options.callbacks.onUpdate);
-		tween.onComplete(options.callbacks.onComplete);
-		if(autostart) tween.start();
+		tween.onUpdate(config.options.callbacks.onUpdate);
+		tween.onComplete(config.options.callbacks.onComplete);
+		if(config.autostart) tween.start();
 
 		function onUpdate(){
-			for(var key in target) propRec(elem, key, start[key]);
+			for(var key in config.to) propRec(config.element, key, config.from[key]);
 		};
+
+		function onComplete(){};
 
 		function propRec(context, route, value){
 			var paths = route.split('.');
@@ -60,9 +62,49 @@
 		return tween;
 	}
 
-	function Stagger(elements, tween, delay){
-		for(var i = 0; i < elements.length; i++){
+	function Stagger(tweens, delay, callback){
+		var tween;
+		for(var i = 0; i < tweens.length; i++){
+			tweens[i].autostart = false;
+			tween = TWEEN.Motion(tweens[i]);
+			tween.delay(i * delay);
 
+			if(i === tweens.length - 1) tween.onComplete(callback);
+
+			tween.start();
 		}
 	}
+
+	function Batch(elements, config, delay, callback){
+		var tween;
+
+		delay = delay || 0;
+		callback = callback || function(){};
+		config.autostart = false;
+
+		
+		for(var i = 0; i < elements.length; i++){
+			var _config = clone(config);
+			_config.element = elements[i];
+			_config.options = clone(config.options);
+
+			tween = TWEEN.Motion(_config);
+			tween.delay(i * delay);
+
+			if(i === elements.length - 1) tween.onComplete(callback);
+
+			tween.start();
+		}
+
+	}
+
+	function clone(obj){
+		if (null == obj || "object" != typeof obj) return obj;
+	    var copy = obj.constructor();
+	    for (var attr in obj) {
+	        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+	    }
+	    return copy;
+	}
+
 })();
